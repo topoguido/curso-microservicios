16adeafb048cc8d6a36572333627d584e0b0bd80
Emiliano <emiliano.guido@gmail.com>
2023-03-29 15:09:52 -0300

    Section 2

--- springboot-servicio-item/src/main/java/com/topo/springboot/app/item/models/Producto.java
+++ springboot-servicio-item/src/main/java/com/topo/springboot/app/item/models/Producto.java
@@ -8,6 +8,15 @@ public class Producto {
 	private String nombre;
 	private Double precio;
 	private Date createAt;
+	private Integer port;
+		
+	public Integer getPort() {
+		return port;
+	}
+	public void setPort(Integer port) {
+		this.port = port;
+	}
+	
 	public Long getId() {
 		return id;
 	}
